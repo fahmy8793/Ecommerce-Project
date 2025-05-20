11894aa112e8daf974b449d2fd6db46ca277b961
Mohamed Ramadan <mhmdrmdan652@gmail.com>
2025-05-20 20:44:48 +0300

    add cart and checkout

--- userFeatures/Rfirebase-config.js
+++ userFeatures/Rfirebase-config.js
@@ -5,6 +5,14 @@ const firebaseConfig = {
     storageBucket: "js-project-92c8d.firebasestorage.app",
     messagingSenderId: "629295037407",
     appId: "1:629295037407:web:d929a43135e6131cd4226a"
+    // osama configration
+    // apiKey: "AIzaSyAEpHidRTkCegpQmOzHy6hV7iApvAJIFoM",
+    // authDomain: "test-de06d.firebaseapp.com",
+    // projectId: "test-de06d",
+    // storageBucket: "test-de06d.firebasestorage.app",
+    // messagingSenderId: "308036633924",
+    // appId: "1:308036633924:web:1894faeee93a0815b14311",
+    // measurementId: "G-XKSNCZ34EL",
 };
 firebase.initializeApp(firebaseConfig);
 
